0794d3b9403e5eed55726c276591cf9813b8b5e8
Alek30k <alemank23k@gmail.com>
2024-10-28 14:35:07 -0300

    last commit

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-// import Menu from "@/components/Menu";
+import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
@@ -20,7 +20,7 @@ export default function DashboardLayout({
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold">SchooLama</span>
         </Link>
-        {/* <Menu /> */}
+        <Menu />
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
